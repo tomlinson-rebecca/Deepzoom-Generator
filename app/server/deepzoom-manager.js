e2c74d201f4fd904a6e36242e40c26c956b7a88e
Side Li <lisided@gmail.com>
2016-05-07 23:54:35 -0700

    Enabled uploading zipped collection

--- app/server/deepzoom-manager.js
+++ app/server/deepzoom-manager.js
@@ -1,3 +1,4 @@
+require('shelljs/global');
 var exports = module.exports;
 var fs = require('fs'); //file system
 var fsx = require('fs-extra');
@@ -5,12 +6,114 @@ var sharp = require('sharp');
 var archiver = require('archiver');
 
 
+var id;
+var dir;
+
+var generateOne = function(newPath, desFile, callback){
+	//convert only one image
+	sharp(newPath).tile(256).toFile(desFile,
+			function(error, info){
+		if(error){
+			console.log("Error here");
+				callback(error);
+		}else{
+			var output = fs.createWriteStream(__dirname + "/../public/collection/" + id + '.zip');
+			var zipArchive = archiver('zip');
+
+			output.on('close', function() {
+					console.log(zipArchive.pointer() + ' total bytes');
+					console.log('archiver has been finalized and the output file descriptor has closed.');
+			});
+
+			zipArchive.on('error', function(err) {
+					throw err;
+			});
+
+			zipArchive.pipe(output);
+
+			zipArchive.bulk([
+			    { src: ['**/*'], cwd: dir, expand: true }
+			])
+			zipArchive.finalize();
+			callback(null);
+		}
+	});
+};
+
+var tileImage = function(newPath, desFile, callback){
+	sharp(newPath).tile(256).toFile(desFile,
+			function(error, info){
+		if(error){
+			console.log("Error here");
+				callback(error);
+		}else{
+			callback(null);
+		}
+	});
+};
+
+var generateBatch = function(newPath, callback){
+	var readStream = fs.createReadStream(newPath);
+	var currentDir = __dirname;
+	var images = [];
+
+	console.log('newPath: '+ newPath);
+	console.log('dir: ' + dir);
+
+	exec('unzip '+newPath+' -d '+dir);
+	
+	cd(dir);
+	ls('*.*').forEach(function(file) {
+		if(file.split('.').pop() != "zip"){
+			images.push(file);
+		}
+	});
+	cd(currentDir);
+
+
+	for(var i = 0; i < images.length - 1; i ++){
+		var desFile = dir+'/'+images[i].substr(0, images[i].lastIndexOf('.'))+'.dzi';
+
+		tileImage(dir+'/'+images[i], desFile, function(err){
+			if(err) callback(err);
+		});
+	}
+
+	tileImage(dir+'/'+images[images.length-1],
+		dir+'/'+images[images.length-1].substr(0, images[images.length-1].lastIndexOf('.'))+'.dzi',
+		function(err){
+			if (!err){
+				var output = fs.createWriteStream(__dirname + "/../public/collection/" + id + '.zip');
+				var zipArchive = archiver('zip');
+
+				output.on('close', function() {
+						console.log(zipArchive.pointer() + ' total bytes');
+						console.log('archiver has been finalized and the output file descriptor has closed.');
+				});
+
+				zipArchive.on('error', function(err) {
+						throw err;
+				});
+
+				zipArchive.pipe(output);
+
+				zipArchive.bulk([
+				    { src: ['**/*'], cwd: dir, expand: true }
+				])
+				zipArchive.finalize();
+				callback(null);
+			}
+	});
+
+};
+
+
 exports.generateDeepzoom = function(files, callback){
-	var dir = __dirname + "/../public/collection/" + files.file.filename;
+	dir = __dirname + "/../public/collection/" + files.file.filename;
 	fs.mkdirSync(dir);
 	fs.readFile(files.file.path, function(err, data){
 		var newPath = dir + "/" + files.file.originalname;
-		var id = files.file.originalname.substr(0, files.file.originalname.lastIndexOf('.'));
+		id = files.file.originalname.substr(0, files.file.originalname.lastIndexOf('.'));
 		var desFile = dir + "/" + id + '.dzi';
 
 		if(fs.existsSync(desFile)){
@@ -25,37 +128,28 @@ exports.generateDeepzoom = function(files, callback){
 				if(err){
 					callback(null, err);
 				}else{
-					sharp(newPath).tile(256).toFile(desFile,
-	      				function(error, info){
-	        			if(error){
-	        				console.log("Error here");
-	          				callback(null, error);
-	        			}else{
-	        				var output = fs.createWriteStream(__dirname + "/../public/collection/" + id + '.zip');
-							var zipArchive = archiver('zip');
-
-							output.on('close', function() {
-	  							console.log(zipArchive.pointer() + ' total bytes');
-	  							console.log('archiver has been finalized and the output file descriptor has closed.');
-							});
-
-							zipArchive.on('error', function(err) {
-	  							throw err;
-							});
-
-							zipArchive.pipe(output);
-
-							zipArchive.bulk([
-							    { src: ['**/*'], cwd: dir, expand: true }
-							])
-							zipArchive.finalize();
-							callback("collection/"+ id + '.zip', null);
-	        			}
-	    			});
+					var extension = files.file.originalname.split('.').pop();
+					if(extension == 'zip'){
+						generateBatch(newPath, function(e){
+							if(e){
+								callback(null, error);
+							}else{
+								callback("collection/"+ id + '.zip', null);
+							}
+						});
+					}else{
+						generateOne(newPath, desFile, function(e){
+							if(e){
+								callback(null, error);
+							}else{
+								callback("collection/"+ id + '.zip', null);
+							}
+						});
+					}
 				}
 			});		
 		}
-
-
 	});
 };
+
+
